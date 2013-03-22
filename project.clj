29d52e3fdf0590be58f27bd055d812d47728dd04
Alan Dipert <alan@dipert.org>
2013-03-21 19:33:41 -0700

    version to 1.1.2
    - moved postgresql-atom to separate ns
    - bumped clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject alandipert/enduro "1.1.1"
+(defproject alandipert/enduro "1.1.2"
   :description "Durable Atoms for Clojure"
   :url "https://github.com/alandipert/enduro"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [postgresql "8.4-702.jdbc4"]
                  [org.clojure/java.jdbc "0.2.3"]])
